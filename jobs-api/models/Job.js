9fac55bd464f6141dbe57d23f40560eec135e88a
Fatemeh <uni.khosravi@gmail.com>
2025-04-11 15:00:00 +0330

    add job and user models, and authentication routes

---
+++ jobs-api/models/Job.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    createdBy: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "please provide user"],
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Job", JobSchema);
